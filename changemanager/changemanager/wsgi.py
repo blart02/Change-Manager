f79d4eca7e96b570a9f6486c9b8963ff4548becb
= <=>
2022-09-25 16:22:33 +0100

    wsgi changes

--- changemanager/changemanager/wsgi.py
+++ changemanager/changemanager/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'changemanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'changemanager.changemanager.settings')
 
 application = get_wsgi_application()
