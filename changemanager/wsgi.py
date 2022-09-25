92ce3e215e2ed68402bf602ed9b3e0b21c36a03e
= <=>
2022-09-25 16:26:54 +0100

    folder structure changes

--- changemanager/wsgi.py
+++ changemanager/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'changemanager.changemanager.settings')
+                      'changemanager.settings')
 
 application = get_wsgi_application()
